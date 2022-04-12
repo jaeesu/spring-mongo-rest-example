872fee2ffebd7da5f38d562e0bfea665243847ab
jaeesu <wogmlsu@gmail.com>
2022-04-12 21:44:56 +0900

    feat: mongoDB 환경 구성

---
+++ src/main/java/com/example/mongodemo/configuration/MongoConfiguration.java
@@ -0,0 +1,33 @@
+package com.example.mongodemo.configuration;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories
+public class MongoConfiguration {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String uri;
+
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+
+    public MongoClient mongoClient() {
+        ConnectionString connectionString = new ConnectionString(uri);
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), uri);
+    }
+}
